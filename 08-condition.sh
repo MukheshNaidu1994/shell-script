88b2cd9ffc924d823372ef6891c44af4fcce4388
MukheshNaidu1994 <hanumanathmukhesh@gmail.com>
2024-09-04 13:10:34 +0530

    commited

--- 08-condition.sh
+++ 08-condition.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 id=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 if [ $id -ne 0 ]
 then
-  echo " error: could you please provide root access "
+  echo -e " $R error: $N could you please provide root access "
 else 
-  echo " you have a root access "
+  echo -e " you have a $G root access "
 fi
 
 
