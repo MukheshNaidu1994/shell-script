b805aa41daf0e5f7fb50f01c6ac578a100348b7e
MukheshNaidu1994 <hanumanathmukhesh@gmail.com>
2024-09-04 13:29:28 +0530

    commited

--- 08-condition.sh
+++ 08-condition.sh
@@ -3,12 +3,12 @@
 id=$(id -u)
 
 VALIDATE (){
-if [ $? -ne 0 ]
+if [ $1 -ne 0 ]
 then
- echo -e " installing  is $Y failed "
+ echo -e " $2............ $Y failed "
  exit 1
 else
-  echo -e " installing  is $Y success "
+  echo -e " $2...... $Y success "
 fi
 }
 
@@ -28,9 +28,9 @@ fi
 
 yum install mysql -y
 
-VALIDATE
+VALIDATE $? " insatlling mysql "
 
 yum install git -y
 
-VALIDATE
+VALIDATE $? " insatlling git "
 
